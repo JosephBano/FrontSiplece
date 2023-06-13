1418cf259c42c5b37f1a8ee5c3d7e09d4bd3dcf6
Joseph <andres.jbo2003@gmail.com>
2023-06-13 16:46:07 -0500

    button dashboard y /inicio en app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+
+
 const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full'},
+  { path:'inicio',  component: InicioComponent, pathMatch: 'full'},
   { path:'dashboard', component: DashboardComponent },
-  { path:'', redirectTo:'/dashboard', pathMatch:'full' },
+  { path:'**', redirectTo:'/dashboard', pathMatch:'full' },
 ];
 
 @NgModule({
