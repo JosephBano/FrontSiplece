69d8ba479bd4e864b1a634f2da0a8404ef7f7b2a
Joseph <andres.jbo2003@gmail.com>
2023-06-22 15:10:36 -0500

    Create ModalEvideciasComponent no funcional

--- src/app/components/dashboard/selector/detalle-indicador/modal-evidencias/modal-evidencias.component.ts
+++ src/app/components/dashboard/selector/detalle-indicador/modal-evidencias/modal-evidencias.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-modal-evidencias',
   templateUrl: './modal-evidencias.component.html',
   styleUrls: ['./modal-evidencias.component.css']
 })
-export class ModalEvidenciasComponent {
+export class ModalEvidenciasComponent implements OnInit{
 
+  files: File[] = [];
+
+  constructor(
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  onFileSelected(event: Event) {
+    const element = event.target as HTMLInputElement;
+    if (element.files) {
+      this.files = [...this.files, ...Array.from(element.files)];
+    }
+    element.value = '';
+  }
+  
+  onUpload() {
+    this.toastr.success("Archivos subidos con exito")
+    this.files = []
+  }
 }
