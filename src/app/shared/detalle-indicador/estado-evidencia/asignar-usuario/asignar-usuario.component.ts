a24a991cde8200afab7c7c4c3d2986975406a1b5
Joseph <andres.jbo2003@gmail.com>
2023-09-04 16:49:59 -0500

    Vista asignacion de encargados pt1

--- src/app/shared/detalle-indicador/estado-evidencia/asignar-usuario/asignar-usuario.component.ts
+++ src/app/shared/detalle-indicador/estado-evidencia/asignar-usuario/asignar-usuario.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { filter } from 'rxjs';
+import { ArchivoEvidencia } from 'src/app/models/modelos-generales/archivo-evidencia.model';
 import { Usuario } from 'src/app/models/usuario.model';
+import { ArchivoEvidenciaService } from 'src/app/services/modeloServicios/archivo-evidencia.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
@@ -12,14 +13,20 @@ export class AsignarUsuarioComponent implements OnInit{
   @Input() IdEvidencia: any;
 
   Usuarios: Usuario[] = [];
-  
+  Archivos: ArchivoEvidencia[] = [];
   filter!: string;
 
   constructor(
     private userService: UsuarioService,
+    private archivoService: ArchivoEvidenciaService,
   ) { }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+  
+  loadData() {
     this.userService.getUsuarios().subscribe(data => this.Usuarios = data);  
+    this.archivoService.GetByEvidencia(this.IdEvidencia).subscribe(data => this.Archivos = data);
   }
 }
