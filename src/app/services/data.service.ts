84709bceb8a89b5469bf76cd113a7283878a252a
Joseph <andres.jbo2003@gmail.com>
2023-09-18 15:53:29 -0500

    DataService funciones generales

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { CriteriosService } from './modeloServicios/criterios.service';
+import { SubCriteriosService } from './modeloServicios/sub-criterios.service';
+import { IndicadorService } from './modeloServicios/indicador.service';
+import { ElementoFundamentalService } from './modeloServicios/elemento-fundamental.service';
+import { EvidenciaService } from './modeloServicios/evidencia.service';
+import { Criterio } from '../models/modelos-generales/criterio.model';
+import { toArray, filter } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(
+    private critService: CriteriosService,
+    private subcService: SubCriteriosService,
+    private indiService: IndicadorService,
+    private elemService: ElementoFundamentalService,
+    private evidService: EvidenciaService,
+  ) { }
+
+  /*public async getUniqueCode(idPadre: string , detalle: string, tipo: string){
+    switch(tipo) {
+      case 'criterio':
+        const value = `crit-${idPadre}${this.formatoCadena(detalle)}`;
+        const data = await this.critService.GetDetalleByModelo(idPadre);
+        const cont = data.pipe(filter(x => x === detalle));
+        if (cont.length > 0) {
+          return `${value}${cont}`;
+        }
+        return value;
+        break;
+      case 'subcriterio':
+        this.subcService.getByCriterio(idPadre).subscribe(
+          (data) => {
+          }
+        )
+    }
+  }*/
+
+  formatoCadena(cadena: string): string {
+    const primeros = cadena.slice(0, 2);
+    const ultimos = cadena.slice(-2);
+    return primeros + ultimos;
+  }
+}
