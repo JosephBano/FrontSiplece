4b77a8356548465f057494f3051cdbd517122b4b
Joseph <andres.jbo2003@gmail.com>
2023-09-21 17:16:51 -0500

    redirect to /inicio on pathmatch full''

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { PanelComponent } from './components/panel/panel.component';
 import { authGuard } from './helpers/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/panel', pathMatch: 'full'},
+  { path: '', redirectTo: '/inicio', pathMatch: 'full'},
   { path:'inicio',  component: InicioComponent, loadChildren: () => import('./components/inicio/inicio.module').then(m => m.InicioModule)},
   { path: 'panel', component: PanelComponent, canActivate: [authGuard], loadChildren: () => import('./components/panel/panel.module').then(m => m.PanelModule)},
   { path: '**', redirectTo: '/panel'}
