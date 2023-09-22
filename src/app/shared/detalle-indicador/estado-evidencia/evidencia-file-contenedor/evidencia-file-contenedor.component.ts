15da3a4e1f0c87efc5ae381e8a664c00423cf134
diego40g <diegopaznaula@hotmail.com>
2023-09-22 07:43:21 -0500

    adding functions to disable and request to change the status

--- src/app/shared/detalle-indicador/estado-evidencia/evidencia-file-contenedor/evidencia-file-contenedor.component.ts
+++ src/app/shared/detalle-indicador/estado-evidencia/evidencia-file-contenedor/evidencia-file-contenedor.component.ts
@@ -3,7 +3,7 @@ import { ArchivoEvidencia } from '../../../../models/modelos-generales/archivo-e
 import { ArchivoEvidenciaService } from 'src/app/services/modeloServicios/archivo-evidencia.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoginService } from 'src/app/services/login.service';
-
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-evidencia-file-contenedor',
   templateUrl: './evidencia-file-contenedor.component.html',
@@ -14,6 +14,8 @@ export class EvidenciaFileContenedorComponent implements OnInit{
   
   Archivos: ArchivoEvidencia[] = [];
   radiobuton!: FormGroup;
+  formDisabled: boolean[] = [];
+  formStatus: string[] = [];
   
   //rolviewradios = '1';
   //rolviewradios = '2';
@@ -23,6 +25,7 @@ export class EvidenciaFileContenedorComponent implements OnInit{
     private archivoService: ArchivoEvidenciaService,
     private fb: FormBuilder,
     private loginService: LoginService,
+    private toastr: ToastrService
   ) {
     this.radiobuton = this.fb.group({
       estado: ['0', [Validators.required]] 
@@ -35,7 +38,67 @@ export class EvidenciaFileContenedorComponent implements OnInit{
 
   data(): void {
     this.archivoService.GetByEvidenciaUser(this.IdEvidencia,this.loginService.getTokenDecoded().usuarioRegistra).subscribe(data =>{
-      this.Archivos = data
+      this.Archivos = data      
+      this.formDisabled = new Array(this.Archivos.length).fill(false);
+      this.formStatus = new Array(this.Archivos.length).fill('btnWait');
     });
+  }
+
+  sendValidated(i:number){    
+    console.log(this.radiobuton.value.estado);
+    if(this.radiobuton.value.estado!=='0'){
+      this.formDisabled[i]=true;
+      this.formStatus[i]=this.getClassStyle(this.radiobuton.value.estado);
+      this.updateStatus(i,this.radiobuton.value.estado);
+      this.radiobuton.value.estado='0';
+    }
+  }
+
+   getClassStyle(value: string): string{
+    if(value==='0'){
+      return 'btnWait'
+    }
+    if(value==='1'){
+      return 'btnDisapprove'
+    }
+    return 'btnApprove'
+  }
+  async updateStatus(index:number,value: string): Promise<void>{
+    const fileUpdate: ArchivoEvidencia = {
+      IdArchivoEvidencia:this.Archivos[index].IdArchivoEvidencia,
+      IdEvidencia:this.Archivos[index].IdEvidencia,
+      IdPeriodo:this.Archivos[index].IdPeriodo,
+      Estado:value,
+      FechaRegistro:this.Archivos[index].FechaRegistro,
+      FechaValidacion:this.obtenerFechaEnFormato(),
+      UsuarioRegistra:this.Archivos[index].UsuarioRegistra,
+      RolValida:this.Archivos[index].RolValida,
+      Detalle:this.Archivos[index].Detalle,
+      PathUrl:this.Archivos[index].PathUrl,
+      Activo:this.Archivos[index].Activo
+    }
+    console.log(fileUpdate);
+    
+    try{
+      this.archivoService.UpdateArchivo(fileUpdate).subscribe(data=>{
+        if(data.Estado==value){
+          this.toastr.success("Archivo evaluado con exito");
+        }
+      });
+    }catch(error){ 
+        this.toastr.error("Error al evaluar el archivo");
+    }
+  }
+  obtenerFechaEnFormato() {
+    const fecha = new Date();
+    const anio = fecha.getFullYear();
+    const mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
+    const dia = fecha.getDate().toString().padStart(2, '0'); 
+    const hora = fecha.getHours().toString().padStart(2, '0');
+    const minutos = fecha.getMinutes().toString().padStart(2, '0');
+    const segundos = fecha.getSeconds().toString().padStart(2, '0');
+
+  
+    return `${anio}-${mes}-${dia}T${hora}:${minutos}:${segundos}`;
   }
 }
