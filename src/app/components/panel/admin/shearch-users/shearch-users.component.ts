d2f90a833e48d91c38c4291e522b0cf3739619e6
diego40g <diegopaznaula@hotmail.com>
2023-09-28 16:41:15 -0500

    adding the user and rol to manage permissions

--- src/app/components/panel/admin/shearch-users/shearch-users.component.ts
+++ src/app/components/panel/admin/shearch-users/shearch-users.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Usuario } from 'src/app/models/usuario.model';
 import { DataService } from 'src/app/services/data.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
@@ -16,23 +16,24 @@ export class ShearchUsersComponent {
   UsuariosSelected!: Usuario;
   Perfiles: any[]=[{rol:'SUPERVISOR',id:1}, {rol:'ADMIN', id:2}, {rol:'ENCARGADO', id:3}, {rol:'SUPADMIN',id:4}];
   
-  agregarEncargado!: FormGroup;
-  perfil!: FormGroup;
+  valueEncargadoFilter: string = '';
+  valuePerfilFilter: string = '';
 
-  filter!: string;
   mostrarUsuarios: boolean = false;
   statfilter: string = '';
+
+  selects: FormGroup;
+  disabledButton = true;
+  displayPermissions = false;
 
   constructor(
     private usuarios: UsuarioService,
     private fb: FormBuilder,
     private ds: DataService,
   ) { 
-    this.agregarEncargado = this.fb.group({
-      nombre: '',
-    })
-    this.perfil = this.fb.group({
-      perfil: '0',
+    this.selects = this.fb.group({
+      filterEncargado: new FormControl({value: '0', disabled: false}),
+      filterPerfil: new FormControl({value: '0', disabled: true})
     })
   }
 
@@ -48,10 +49,55 @@ export class ShearchUsersComponent {
     )
   }
 
+  OnChangeEncargadoFilter() {
+    this.displayPermissions = false;
+    this.valueEncargadoFilter = this.selects.get('filterEncargado')?.value;
+    if(this.valueEncargadoFilter!=='0') {      
+      this.selects.get("filterPerfil")?.enable();
+      this.selects.get("filterPerfil")?.setValue('0');
+    } else {
+      this.selects.get("filterPerfil")?.disable();
+    }
+    this.OnChangePerfilFilter();
+  }
+  OnChangePerfilFilter() {
+    this.displayPermissions = false;
+    this.valuePerfilFilter = this.selects.get('filterPerfil')?.value;
+        
+    if(this.valuePerfilFilter === '0' || this.valueEncargadoFilter === '0') {
+      this.disabledButton = true;
+      this.selects.get('filterPerfil')?.setValue('')
+    } else {
+      this.disabledButton = false;
+    }
+  }
+
+  pressTrashEncargado(){
+    this.selects.get('filterEncargado')!.setValue('0'); 
+    this.valueEncargadoFilter = '0'; 
+    this.disabledButton = true;
+    this.selects.get("filterPerfil")?.disable();
+  }
+  pressTrashPerfil(){
+    this.selects.get('filterPerfil')!.setValue('0');
+    this.valuePerfilFilter = '0'; 
+    this.disabledButton = true;
+  }
 
   ChangeVisibilityUsers() {
     console.log('selected');
     
     this.mostrarUsuarios = true;
+  }
+
+  notIsCeroValidator(control: FormControl) {
+    if (control.value === 0) {
+      return { noEsCero: true };
+    }
+    return null;
+  }  
+
+  onSubmit() { 
+    this.displayPermissions = true;
   }
 }
